6e78a12ae5e15c145a8a19c6364a8d2cc74fa8a8
Cameron Foskett <cameron.foskett@ibm.com>
2024-02-23 13:59:18 +0000

    Initial Commit

---
+++ Intereactive Premise Management Tool/Intereactive_Premise_Management_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intereactive_Premise_Management_ToolApp.swift
+//  Intereactive Premise Management Tool
+//
+//  Created by Cameron Foskett on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Intereactive_Premise_Management_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
